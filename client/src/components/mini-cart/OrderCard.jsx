941a467835bdff364afb2fc20426d761d476bbf4
kayla <kayla3445@yahoo.com>
2023-08-13 20:15:22 -0500

    mini cart for tablet/desktop done

--- client/src/components/mini-cart/OrderCard.jsx
+++ client/src/components/mini-cart/OrderCard.jsx
@@ -1,16 +1,17 @@
-import styles from "./OrderCard.module.css";
-import MiniCakeCard from "./MiniCakeCard";
-import Qty from "./Qty";
-import Size from "./Size";
+import styles from './OrderCard.module.css';
+import MiniCakeCard from './MiniCakeCard';
+import Qty from './Qty';
+import Size from './Size';
 
 export default function OrderCard() {
   return (
     <div className={styles.cardContainer}>
       <MiniCakeCard />
-      <Qty />
-      <Size />
-      <p className={styles.paragraph}>$18.19</p>
-      <p className={styles.paragraph}>$18.19</p>
+      <div className={styles.qtySizeWrap}>
+        <Qty />
+        <Size />
+        <p className={styles.paragraph}>Price: $198.19</p>
+      </div>
     </div>
   );
 }
--- client/src/components/mini-cart/Size.jsx
+++ client/src/components/mini-cart/Size.jsx
@@ -1,18 +1,20 @@
-import styles from "./Size.module.css";
-import { useState } from "react";
+import styles from './Size.module.css';
+import { useState } from 'react';
 
 export default function Size() {
   const [size, setSize] = useState(6);
 
   return (
     <div className={styles.sizeContainer}>
+      <p className={styles.label}>Size:</p>
+
       <select
         onChange={(e) => setSize(e.target.value)}
         value={size}
         className={styles.sizeSelect}
       >
-        <option value="6">6&quot;</option>
-        <option value="9">9&quot;</option>
+        <option value='6'>6&quot;</option>
+        <option value='9'>9&quot;</option>
       </select>
     </div>
   );
