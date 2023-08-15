7b13d00204f3fc8d04fcf470dce9ffcf65af61a4
kayla <kayla3445@yahoo.com>
2023-08-15 12:01:23 -0500

    my-account and admin/orders/id

--- client/src/screens/MyAccountScreen.jsx
+++ client/src/screens/MyAccountScreen.jsx
@@ -4,24 +4,32 @@ import MyAccountForm from '../components/my-account/MyAccountForm';
 import MyAccountOrders from '../components/my-account/MyAccountOrders';
 
 import backgroundCake1 from '../assets/images/mobile/my-account-cheesecake-mobile-199w.png';
+import backgroundCakeLarge from '../assets/images/tablet/account-cake-443w.png';
 
 import backgroundCake2 from '../assets/images/mobile/cart-cake.png';
 
 export default function MyAccountScreen() {
   return (
     <div className={styles.accountScreen}>
-      <img
-        src={backgroundCake1}
-        alt='cheesecake'
-        className={styles.backgroundCake1}
-      />
+      <picture>
+        <source media='(min-width: 744px)' srcSet={backgroundCakeLarge} />
+
+        <img
+          src={backgroundCake1}
+          alt='cheesecake'
+          className={styles.backgroundCake1}
+        />
+      </picture>
 
       <h1 className={styles.accountHeader}>My Account</h1>
 
-      <MyAccountForm />
-      <h2 className={styles.accountHeader}>My Orders</h2>
-
-      <MyAccountOrders />
+      <div className={styles.flexWrap}>
+        <MyAccountForm />
+        <div>
+          <h2 className={styles.orderHeader}>My Orders</h2>
+          <MyAccountOrders />
+        </div>
+      </div>
 
       <img
         src={backgroundCake2}
