fa61fdcf858970be40169bbdbcaed3ecb40f3daf
kayla <kayla3445@yahoo.com>
2023-08-15 20:41:10 -0500

    admin orders

--- client/src/screens/Admin/AdminOrderScreen.jsx
+++ client/src/screens/Admin/AdminOrderScreen.jsx
@@ -1,8 +1,10 @@
 import styles from './AdminOrderScreen.module.css';
 import MyAccountOrders from '../../components/my-account/MyAccountOrders';
+import AdminBackLink from '../../components/admin/AdminBackLink';
 export default function AdminOrderScreen() {
   return (
     <div className={styles.container}>
+      <AdminBackLink to='orders' />
       <h1 className={styles.heading}>Order Details</h1>
       <MyAccountOrders />
     </div>
