287b6f9ef9fde5f262bd6da4910b07df86084b39
kayla <kayla3445@yahoo.com>
2023-09-28 20:11:21 -0500

    edited server static path

--- server/server.js
+++ server/server.js
@@ -35,9 +35,9 @@ const app = express();
 // serve static files
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static(path.join(__dirname, '/client/dist')));
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
   );
 }
 
